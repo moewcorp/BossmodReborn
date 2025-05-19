9626c8b2638a576c103f8d3c559088d3b5d3cdef
CarnifexOptimus <156172553+CarnifexOptimus@users.noreply.github.com>
2025-05-19 21:05:09 +0200

    added M7S tendrils config

---
+++ BossMod/Modules/Dawntrail/Savage/M07SBruteAbombinator/M07SBruteAbombinatorConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace BossMod.Dawntrail.Savage.M07SBruteAbombinator;
+
+[ConfigDisplay(Order = 0x120, Parent = typeof(DawntrailConfig))]
+public class M07SBruteAbombinatorConfig() : ConfigNode()
+{
+    [PropertyDisplay("Disable seed AOE prediction")]
+    public bool EnableSeedPrediction = true;
+}
