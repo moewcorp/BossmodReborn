2ca04c45ede3ab2edf629c81239a721959a2584d
CarnifexOptimus <156172553+CarnifexOptimus@users.noreply.github.com>
2025-06-29 11:16:46 +0200

    Saunion & Dawon module

--- BossMod/Modules/Shadowbringers/Foray/TheDalriada/DAL3SaunionDawon/DAL3SaunionDawon.cs
+++ BossMod/Modules/Shadowbringers/Foray/TheDalriada/DAL3SaunionDawon/DAL3SaunionDawon.cs
@@ -1,37 +1,50 @@
 ﻿namespace BossMod.Shadowbringers.Foray.TheDalriada.DAL3SaunionDawon;
 
-class HighPoweredMagitekRay(BossModule module) : Components.SingleTargetCast(module, (uint)AID.HighPoweredMagitekRay);
-class ToothAndTalon(BossModule module) : Components.SingleTargetCast(module, (uint)AID.ToothAndTalon);
-class PentagustAOE(BossModule module) : Components.SimpleAOEs(module, (uint)AID.PentagustAOE, new AOEShapeCone(50, 10.Degrees()));
-class RawHeat(BossModule module) : Components.SimpleAOEs(module, (uint)AID.RawHeat, 10);
-class SurfaceMissile(BossModule module) : Components.SimpleAOEs(module, (uint)AID.SurfaceMissile, 6);
-class SwoopingFrenzyAOE(BossModule module) : Components.SimpleAOEs(module, (uint)AID.SwoopingFrenzyAOE, 12);
-class Touchdown3(BossModule module) : Components.RaidwideCast(module, (uint)AID.Touchdown3);
-class MissileSalvo(BossModule module) : Components.SpreadFromCastTargets(module, (uint)AID.MissileSalvo, 6);
-class MagitekCrossray(BossModule module) : Components.SimpleAOEs(module, (uint)AID.MagitekCrossray, new AOEShapeCross(60, 8));
-
-class Donut(BossModule module, uint aid) : Components.SimpleAOEs(module, aid, new AOEShapeDonut(7, 60));
-class MagitekHalo(BossModule module) : Donut(module, (uint)AID.MagitekHalo);
-class FrigidPulseAOE(BossModule module) : Donut(module, (uint)AID.FrigidPulseAOE);
-
-class AntiPersonnelMissile(BossModule module) : Components.SpreadFromCastTargets(module, (uint)AID.AntiPersonnelMissile, 6);
-
-[ModuleInfo(BossModuleInfo.Maturity.WIP, Contributors = "The Combat Reborn Team", GroupType = BossModuleInfo.GroupType.CriticalEngagement, GroupID = 778, NameID = 32, SortOrder = 4)] //BossNameID = 10192 & 10006
-public class DAL3SaunionDawon : BossModule
+sealed class HighPoweredMagitekRay(BossModule module) : Components.BaitAwayCast(module, (uint)AID.HighPoweredMagitekRay, 5f, tankbuster: true)
 {
-    public readonly List<Actor> Boss;
-    public readonly List<Actor> Dawon;
+    public override bool KeepOnPhaseChange => true;
+}
+sealed class ToothAndTalon(BossModule module) : Components.SingleTargetCast(module, (uint)AID.ToothAndTalon);
+sealed class Pentagust(BossModule module) : Components.SimpleAOEs(module, (uint)AID.Pentagust, new AOEShapeCone(50f, 10f.Degrees()));
+sealed class SurfaceMissile(BossModule module) : Components.SimpleAOEs(module, (uint)AID.SurfaceMissile, 6f)
+{
+    public override bool KeepOnPhaseChange => true;
+}
+sealed class SwoopingFrenzy(BossModule module) : Components.SimpleAOEs(module, (uint)AID.SwoopingFrenzy, 12f);
+sealed class MissileSalvo(BossModule module) : Components.StackWithCastTargets(module, (uint)AID.MissileSalvo, 6f)
+{
+    public override bool KeepOnPhaseChange => true;
+}
+sealed class MagitekCrossray(BossModule module) : Components.SimpleAOEs(module, (uint)AID.MagitekCrossray, MobileHaloCrossray.Cross);
+sealed class MagitekHalo(BossModule module) : Components.SimpleAOEs(module, (uint)AID.MagitekHalo, MobileHaloCrossray.Donut);
+sealed class FrigidPulse(BossModule module) : Components.SimpleAOEs(module, (uint)AID.FrigidPulse, Obey.Donut);
+sealed class AntiPersonnelMissile(BossModule module) : Components.SpreadFromCastTargets(module, (uint)AID.AntiPersonnelMissile, 6f)
+{
+    public override bool KeepOnPhaseChange => true;
+}
+sealed class Touchdown(BossModule module) : Components.SimpleKnockbacks(module, (uint)AID.Touchdown1, 30f, stopAtWall: true);
+sealed class WildfireWinds(BossModule module) : Components.RaidwideCast(module, (uint)AID.WildfireWinds1);
 
-    public DAL3SaunionDawon(WorldState ws, Actor primary) : base(ws, primary, new(650f, -659f), new ArenaBoundsSquare(26.5f))
+[ModuleInfo(BossModuleInfo.Maturity.Verified, Contributors = "The Combat Reborn Team (Malediktus, LTS)", GroupType = BossModuleInfo.GroupType.TheDalriada, GroupID = 778, NameID = 10192, SortOrder = 4)]
+public sealed class DAL3SaunionDawon(WorldState ws, Actor primary) : BossModule(ws, primary, new(650f, -659f), new ArenaBoundsSquare(26.5f))
+{
+    private Actor? _bossDawon;
+    public Actor? BossDawon() => _bossDawon;
+
+    protected override void UpdateModule()
     {
-        Boss = Enemies(OID.Boss);
-        Dawon = Enemies(OID.Dawon);
+        // TODO: this is an ugly hack, think how multi-actor fights can be implemented without it...
+        // the problem is that on wipe, any actor can be deleted and recreated in the same frame
+        if (_bossDawon == null)
+        {
+            var b = Enemies((uint)OID.Dawon);
+            _bossDawon = b.Count != 0 ? b[0] : null;
+        }
     }
 
     protected override void DrawEnemies(int pcSlot, Actor pc)
     {
-        Arena.Actors(Boss);
-        Arena.Actors(Dawon);
+        Arena.Actor(_bossDawon);
         Arena.Actor(PrimaryActor);
     }
 }
