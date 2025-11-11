c888326fcd566be06436be19ed1287e73a8fd035
NiGuangOwO <26338524+NiGuangOwO@users.noreply.github.com>
2025-11-11 20:21:34 +0800

    1

--- BossMod/Modules/Global/DeepDungeon/Config.cs
+++ BossMod/Modules/Global/DeepDungeon/Config.cs
@@ -24,7 +24,7 @@ public sealed class AutoDDConfig : ConfigNode
     public bool Enable = true;
     [PropertyDisplay("启用小地图")]
     public bool EnableMinimap = true;
-    [PropertyDisplay("尽量避开陷阱", tooltip: "避开PalacePal数据中已知的陷阱位置。无需安装PalacePal，因为BMR中已包含这些数据。（无论此设置如何，使用“全景”发现的陷阱都会被避开。）")]
+    [PropertyDisplay("自动躲避陷阱", tooltip: "避开PalacePal数据中已知的陷阱位置。已与在线数据打通，请安装同仓库下PalacePal版本一同使用。（无论此设置如何，使用“全景”发现的陷阱都会被避开。）")]
     public bool TrapHints = true;
     [PropertyDisplay("自动导航至传送装置")]
     public bool AutoPassage = true;
