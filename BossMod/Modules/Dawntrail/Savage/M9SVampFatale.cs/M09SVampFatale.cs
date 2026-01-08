dbb5660cba5c24ad5460a08a9f56aa4342cafe09
Sean Pryor <spryor@redhat.com>
2026-01-08 15:54:00 -0500

    Add a few M9S abilities
    It's mostly copied over from M9N, I lack an enrage replay to work from,
    but hopefully will help someone with more exp get started.

---
+++ BossMod/Modules/Dawntrail/Savage/M9SVampFatale.cs/M09SVampFatale.cs
@@ -0,0 +1,113 @@
+﻿namespace BossMod.Modules.Dawntrail.Savage.M09SVampFatale;
+
+sealed class KillerVoice(BossModule module) : Components.RaidwideCast(module, (uint)AID.KillerVoice);
+
+sealed class HalfMoon(BossModule module) : Components.SimpleAOEGroups(module, [(uint)AID.HalfMoon1, (uint)AID.HalfMoon2, (uint)AID.HalfMoon3, (uint)AID.HalfMoon4, (uint)AID.HalfMoon5, (uint)AID.HalfMoon6, (uint)AID.HalfMoon7, (uint)AID.HalfMoon8], new AOEShapeCone(64f, 90f.Degrees()), 1);
+
+sealed class VampStomp(BossModule module) : Components.SimpleAOEs(module, (uint)AID.VampStomp1, 10f);
+
+sealed class Hardcore(BossModule module) : Components.SpreadFromCastTargets(module, (uint)AID.Hardcore1, 6f);
+
+sealed class Hardcore2(BossModule module) : Components.SpreadFromCastTargets(module, (uint)AID.Hardcore2, 15f);
+
+sealed class FlayingFry(BossModule module) : Components.SpreadFromCastTargets(module, (uint)AID.FlayingFry1, 5f);
+
+sealed class CoffinFiller(BossModule module) : Components.SimpleAOEGroups(module, [(uint)AID.Coffinfiller, (uint)AID.Coffinfiller2, (uint)AID.Coffinfiller3], new AOEShapeRect(32f, 2.5f), 2);
+
+sealed class BlastBeat(BossModule module) : Components.SimpleAOEs(module, (uint)AID.BlastBeat, 8f); // Needs rework to give warning based on static spawns
+
+sealed class DeadWake(BossModule module) : Components.SimpleAOEs(module, (uint)AID.DeadWake1, new AOEShapeRect(10f, 10f));
+
+sealed class PenetratingPitch(BossModule module) : Components.StackWithCastTargets(module, (uint)AID.PenetratingPitch1, 5f, 2, 4);
+
+// Can't seem to make this wait for the BrutalRain1 casts and then vanish, so erring on the side of at least getting the stack in place for the first hit and hoping people will stay.
+sealed class BrutalRain(BossModule module) : Components.StackWithIcon(module, (uint)IconID.BrutalRain, (uint)AID.BrutalRain, 6f, 0d);
+
+sealed class UltrasonicSpread(BossModule module) : Components.BaitAwayCast(module, aid: (uint)AID.UltrasonicSpread, shape: new AOEShapeCone(60f, 15f.Degrees()), endsOnCastEvent: true);
+sealed class UltrasonicSpreadTankbuster(BossModule module) : Components.BaitAwayCast(module, aid: (uint)AID.UltrasonicSpread2, shape: new AOEShapeCone(60f, 45f.Degrees()), endsOnCastEvent: true, tankbuster: true);
+sealed class UltrasonicAmp(BossModule module) : Components.Adds(module, (uint)OID.UltrasonicAmp);
+
+sealed class CharnelCells(BossModule module) : Components.Adds(module, (uint)OID.CharnelCell);
+
+sealed class DeadlyDoornail(BossModule module) : Components.Adds(module, (uint)OID.DeadlyDoornail);
+sealed class FatalFlail(BossModule module) : Components.Adds(module, (uint)OID.FatalFlail);
+
+sealed class CrowdKill(BossModule module) : Components.RaidwideCast(module, (uint)AID.CrowdKill); // Doesn't seem to like this, might need to do after yell?
+
+sealed class HellInACell(BossModule module) : Components.CastTowers(module, (uint)AID.HellInACell, 3f);
+
+sealed class FinaleFatale(BossModule module) : Components.RaidwideCast(module, (uint)AID.FinaleFatale);
+sealed class Enrage(BossModule module) : Components.RaidwideCast(module, (uint)AID.FinaleFatale2, hint: "Enrage");
+
+sealed class PulpingPulse(BossModule module) : Components.SimpleAOEs(module, (uint)AID.PulpingPulse, 5f);
+
+sealed class AetherlettingHit(BossModule module) : Components.SpreadFromCastTargets(module, (uint)AID.Aetherletting1, 6f);
+
+sealed class AetherlettingCross(BossModule module) : Components.SimpleAOEs(module, (uint)AID.Aetherletting2, new AOEShapeCross(40f, 5f));
+
+sealed class InsatiableThirst(BossModule module) : Components.RaidwideCast(module, (uint)AID.InsatiableThirst);
+
+sealed class Plummet(BossModule module) : Components.CastTowers(module, (uint)AID.Plummet, 3f);
+
+sealed class SadisticScreech(BossModule module) : Components.RaidwideCast(module, (uint)AID.SadisticScreech);
+
+sealed class NeckBiter(BossModule module) : Components.Voidzone(module, 3f, GetNeckbiters, 2f)
+{
+    private static Actor[] GetNeckbiters(BossModule module)
+    {
+        var enemies = module.Enemies((uint)OID.Neckbiter);
+        var count = enemies.Count;
+        var index = 0;
+        var neckbiters = new Actor[count];
+        for (var i = 0; i < count; i++)
+        {
+            var z = enemies[i];
+            if (z.EventState != 7)
+            {
+                neckbiters[index++] = z;
+            }
+        }
+        return neckbiters;
+
+    }
+}
+
+sealed class CoffinMaker(BossModule module) : Components.Voidzone(module, 3f, GetCoffinMakers, 7f)
+{
+    private static Actor[] GetCoffinMakers(BossModule module)
+    {
+        var enemies = module.Enemies((uint)OID.Coffinmaker1);
+        var count = enemies.Count;
+        var index = 0;
+        var coffinmakers = new Actor[count];
+        for (var i = 0; i < count; i++)
+        {
+            var z = enemies[i];
+            if (z.EventState != 7)
+            {
+                coffinmakers[index++] = z;
+            }
+        }
+        return coffinmakers;
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP,
+StatesType = typeof(M09VampFataleStates),
+ConfigType = null, // replace null with typeof(VampFataleConfig) if applicable
+ObjectIDType = typeof(OID),
+ActionIDType = typeof(AID), // replace null with typeof(AID) if applicable
+StatusIDType = null, // replace null with typeof(SID) if applicable
+TetherIDType = null, // replace null with typeof(TetherID) if applicable
+IconIDType = typeof(IconID), // replace null with typeof(IconID) if applicable
+PrimaryActorOID = (uint)OID.VampFatale,
+Contributors = "HerStolenLight, Xaenalt",
+Expansion = BossModuleInfo.Expansion.Dawntrail,
+Category = BossModuleInfo.Category.Savage,
+GroupType = BossModuleInfo.GroupType.CFC,
+GroupID = 1068u,
+NameID = 14300u,
+SortOrder = 1,
+PlanLevel = 0)]
+[SkipLocalsInit]
+public sealed class VampFatale(WorldState ws, Actor primary) : BossModule(ws, primary, new(100f, 100f), new ArenaBoundsSquare(20f));
