32a44a0583f0b97fdd51004c5d73ef85bdae90ed
HerStolenLight <113465818+HerStolenLight@users.noreply.github.com>
2026-01-07 21:27:30 -0700

    M9N Fixes

--- BossMod/Modules/Dawntrail/Raid/M09NVampFatale/ArenaChanges.cs
+++ BossMod/Modules/Dawntrail/Raid/M09NVampFatale/ArenaChanges.cs
@@ -19,17 +19,45 @@ sealed class ArenaChanges(BossModule module) : Components.GenericAOEs(module)
         }
     }
 
+    private float length = 20f;
+    private float centerz = 100f;
+    private int wakecount = 0;
+
+    // Each time Dead Wake goes off, we need to shorten the arena by 10 and shift our center -5 Z
+    public override void OnCastFinished(Actor caster, ActorCastInfo spell)
+    {
+        if (spell.Action.ID == (uint)AID.DeadWake1)
+        {
+            wakecount += 1;
+            if (wakecount < 4) // that said if the 4th one happens that'd remove the arena entirely, let's not do that.
+            {
+                length -= 5f;
+                centerz += 5f;
+                Arena.Bounds = new ArenaBoundsRect(10f, length);
+                Arena.Center = new WPos(100f, centerz);
+            }
+        }
+    }
+
     public override void OnMapEffect(byte index, uint state)
     {
         if (index == 0x00 && state == 0x00020001u)
         {
             Arena.Bounds = new ArenaBoundsRect(10f, 20f);
             _aoe = [];
+            //Set our wake variables when we enter or leave the smaller arena, just in case.
+            length = 20f;
+            centerz = 100f;
+            wakecount = 0;
         }
         else if ((index == 0x00 || index == 0x10) && state == 0x00080004u)
         {
             Arena.Bounds = new ArenaBoundsSquare(20f);
             _aoe = [];
+            //Set our wake variables when we enter or leave the smaller arena, just in case.
+            length = 20f;
+            centerz = 100f;
+            wakecount = 0;
         }
         else if (index == 0x10 && state == 0x00020001u)
         {
