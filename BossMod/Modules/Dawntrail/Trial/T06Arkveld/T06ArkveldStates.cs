856afd5593e2bdc184238f8d8b18fd62677b7a19
JoeSparkx <90148778+JoeSparkx@users.noreply.github.com>
2026-01-15 15:09:17 +0000

    Arkveld n wip (#8)
    * Update FFXIVClientStructs
    
    * First pass T06
    
    * update
    
    * update
    
    * update

--- BossMod/Modules/Dawntrail/Trial/T06Arkveld/T06ArkveldStates.cs
+++ BossMod/Modules/Dawntrail/Trial/T06Arkveld/T06ArkveldStates.cs
@@ -1,2 +1,35 @@
 namespace BossMod.Dawntrail.Trial.T06Arkveld;
 
+[SkipLocalsInit]
+sealed class GuardianArkveldStates : StateMachineBuilder
+{
+    public GuardianArkveldStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            // Raidwides
+            .ActivateOnEnter<Roar>()
+            .ActivateOnEnter<ForgedFury>()
+            // Major tells
+            .ActivateOnEnter<ChainbladeBlowLines>()
+            .ActivateOnEnter<WyvernsRadianceCleave>()
+            .ActivateOnEnter<GuardianResonanceRect>()
+            .ActivateOnEnter<Rush>()
+            .ActivateOnEnter<Concentric1>()
+            .ActivateOnEnter<Concentric2>()
+            .ActivateOnEnter<Concentric3>()
+            .ActivateOnEnter<Concentric4>()
+            .ActivateOnEnter<WyvernsOuroblade>()
+            .ActivateOnEnter<SteeltailThrust>()
+            // Player mechanics
+            .ActivateOnEnter<WildEnergy>()
+            .ActivateOnEnter<ChainbladeCharge>()
+            .ActivateOnEnter<ResonanceTowerSmall>()
+            .ActivateOnEnter<ResonanceTowerLarge>()
+            // Arena hazards
+            .ActivateOnEnter<CrackedCrystalSmall>()
+            .ActivateOnEnter<CrackedCrystalLarge>()
+            .ActivateOnEnter<WyvernsVengeance>()
+            .ActivateOnEnter<WyvernsWealAOE>()  // the casted rect telegraph
+            .ActivateOnEnter<WyvernsWealPulses>();
+    }
+}
