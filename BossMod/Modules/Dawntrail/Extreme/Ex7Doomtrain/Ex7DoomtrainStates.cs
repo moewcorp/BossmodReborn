9c44308ac3376ca3a1e66f8fdce6ba0960df43e7
Sean Pryor <spryor@redhat.com>
2026-01-14 21:00:10 -0500

    Add a few abilities for Doomtrain EX
    This mainly adds the intermission, there's probably a few issues with
    the state machine, but it gets us a bit closer at least

--- BossMod/Modules/Dawntrail/Extreme/Ex7Doomtrain/Ex7DoomtrainStates.cs
+++ BossMod/Modules/Dawntrail/Extreme/Ex7Doomtrain/Ex7DoomtrainStates.cs
@@ -6,13 +6,20 @@ sealed class Ex7DoomtrainStates : StateMachineBuilder
     public Ex7DoomtrainStates(BossModule module) : base(module)
     {
         DeathPhase(default, SinglePhase)
-            .ActivateOnEnter<ArenaChanges>();
+            .ActivateOnEnter<ArenaChanges>()
+            .Raw.Update = () => Module.Enemies((uint)OID.Aether).Count != 0;
     }
 
     private void SinglePhase(uint id)
     {
         Car1(id, 8.2f);
         Car2(id + 0x10000u, 2.2f);
+        Car3p1(id + 0x20000u, 5.8f);
+        Intermission(id + 0x30000u, 10f);
+        // Car3p2(id + 0x40000u, 2.2f);
+        // Car4(id + 0x50000u, 2.2f);
+        // Car5(id + 0x60000u, 2.2f);
+        // Car6(id + 0x70000u, 2.2f);
         SimpleState(id + 0xFF0000u, 10000f, "???");
     }
 
@@ -88,7 +95,32 @@ sealed class Ex7DoomtrainStates : StateMachineBuilder
             .SetHint(StateMachine.StateHint.Raidwide);
         Targetable(id + 0x90u, false, 0.2f, "Boss untargetable")
             .SetHint(StateMachine.StateHint.DowntimeStart);
-        Targetable(id + 0xA0u, true, 5.8f, "Boss targetable")
+    }
+
+    private void Car3p1(uint id, float delay)
+    {
+        Targetable(id, true, delay, "Boss targetable")
+            .SetHint(StateMachine.StateHint.DowntimeEnd);
+        ComponentCondition<LightningBurst>(id + 0x10u, 2.6f, static comp => comp.NumCasts != 0, "Tankbusters")
+            .DeactivateOnExit<LightningBurst>();
+        Cast(id + 0x10u, (uint)AID.RunawayTrainVisual1, 8.5f, 5f, "Runaway Train Phase Transition");
+        Targetable(id + 0x20u, false, 0.2f, "Boss untargetable")
+            .SetHint(StateMachine.StateHint.DowntimeStart);
+    }
+
+    private void Intermission(uint id, float delay)
+    {
+
+        var Aether = Module.Enemies((uint)OID.Aether);
+        // If I'm reading this correctly, this blocks the state until Aether is dead, and then we can transition back once Doomtrain is targetable?
+        Condition(id + 0x10u, 120, () => Aether.Any(e => e.IsDead), "Aether down", 10000, 1) // note that time is arbitrary (What?)
+            .ActivateOnEnter<AetherocharAetherosote>()
+            .ActivateOnEnter<AetherialRay>()
+            .ActivateOnExit<RunawayTrain>()
+            .DeactivateOnExit<AetherocharAetherosote>()
+            .DeactivateOnExit<AetherialRay>();
+        Targetable(id + 0x20u, true, 5.2f, "Intermission end")
+            .DeactivateOnEnter<RunawayTrain>()
             .SetHint(StateMachine.StateHint.DowntimeEnd);
     }
 }
