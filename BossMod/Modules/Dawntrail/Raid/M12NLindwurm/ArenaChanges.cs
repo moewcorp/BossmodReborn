3ace53225b45dc57e6ee751bcdcec06fb4cf0f66
gynorhino <typemoondb@gmail.com>
2026-01-17 10:59:55 -0500

    fix offset in arena change

--- BossMod/Modules/Dawntrail/Raid/M12NLindwurm/ArenaChanges.cs
+++ BossMod/Modules/Dawntrail/Raid/M12NLindwurm/ArenaChanges.cs
@@ -16,7 +16,7 @@ sealed class ArenaChanges(BossModule module) : BossComponent(module)
                 _rects.Add(new(center - new WDir(-5f, -7.5f), 5f, 7.5f));
                 _rects.Add(new(center - new WDir(5f, 7.5f), 5f, 7.5f));
                 _rects.Add(new(center - new WDir(15f, -7.5f), 5f, 7.5f));
-                Arena.Bounds = new ArenaBoundsCustom([.. _rects], Offset: 0.5f);
+                Arena.Bounds = new ArenaBoundsCustom([.. _rects], Offset: -0.5f);
             }
             // other set of tiles break
             else if (state == 0x02000100u)
@@ -26,13 +26,13 @@ sealed class ArenaChanges(BossModule module) : BossComponent(module)
                 _rects.Add(new(center - new WDir(-5f, 7.5f), 5f, 7.5f));
                 _rects.Add(new(center - new WDir(5f, -7.5f), 5f, 7.5f));
                 _rects.Add(new(center - new WDir(15f, 7.5f), 5f, 7.5f));
-                Arena.Bounds = new ArenaBoundsCustom([.. _rects], Offset: 0.5f);
+                Arena.Bounds = new ArenaBoundsCustom([.. _rects], Offset: -0.5f);
             }
             // arena partial fix through middle
             else if (state is 0x00200010u or 0x08000400u)
             {
                 _rects.Add(new(Arena.Center, 20f, 5f));
-                Arena.Bounds = new ArenaBoundsCustom([.. _rects], Offset: 0.5f);
+                Arena.Bounds = new ArenaBoundsCustom([.. _rects], Offset: -0.5f);
             }
             // arena reset
             else if (state is 0x10000004u or 0x00080004u or 0x00800004u)
