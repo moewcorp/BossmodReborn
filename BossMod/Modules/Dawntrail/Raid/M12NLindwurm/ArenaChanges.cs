e85381f194c70dcc94ace6ee2d3a1a0218b0642d
gynorhino <typemoondb@gmail.com>
2026-01-16 21:17:48 -0500

    additional markers for M09S, config WIP for M09S, AI WIP for M09S and M12N

--- BossMod/Modules/Dawntrail/Raid/M12NLindwurm/ArenaChanges.cs
+++ BossMod/Modules/Dawntrail/Raid/M12NLindwurm/ArenaChanges.cs
@@ -12,27 +12,27 @@ sealed class ArenaChanges(BossModule module) : BossComponent(module)
             if (state == 0x00020001u)
             {
                 var center = Arena.Center;
-                _rects.Add(new(center - new WDir(-15f, 7.5f), 4.95f, 7.45f));
-                _rects.Add(new(center - new WDir(-5f, -7.5f), 4.95f, 7.45f));
-                _rects.Add(new(center - new WDir(5f, 7.5f), 4.95f, 7.45f));
-                _rects.Add(new(center - new WDir(15f, -7.5f), 4.95f, 7.45f));
-                Arena.Bounds = new ArenaBoundsCustom([.. _rects]);
+                _rects.Add(new(center - new WDir(-15f, 7.5f), 5f, 7.5f));
+                _rects.Add(new(center - new WDir(-5f, -7.5f), 5f, 7.5f));
+                _rects.Add(new(center - new WDir(5f, 7.5f), 5f, 7.5f));
+                _rects.Add(new(center - new WDir(15f, -7.5f), 5f, 7.5f));
+                Arena.Bounds = new ArenaBoundsCustom([.. _rects], Offset: 0.5f);
             }
             // other set of tiles break
             else if (state == 0x02000100u)
             {
                 var center = Arena.Center;
-                _rects.Add(new(center - new WDir(-15f, -7.5f), 4.95f, 7.45f));
-                _rects.Add(new(center - new WDir(-5f, 7.5f), 4.95f, 7.45f));
-                _rects.Add(new(center - new WDir(5f, -7.5f), 4.95f, 7.45f));
-                _rects.Add(new(center - new WDir(15f, 7.5f), 4.95f, 7.45f));
-                Arena.Bounds = new ArenaBoundsCustom([.. _rects]);
+                _rects.Add(new(center - new WDir(-15f, -7.5f), 5f, 7.5f));
+                _rects.Add(new(center - new WDir(-5f, 7.5f), 5f, 7.5f));
+                _rects.Add(new(center - new WDir(5f, -7.5f), 5f, 7.5f));
+                _rects.Add(new(center - new WDir(15f, 7.5f), 5f, 7.5f));
+                Arena.Bounds = new ArenaBoundsCustom([.. _rects], Offset: 0.5f);
             }
             // arena partial fix through middle
             else if (state is 0x00200010u or 0x08000400u)
             {
                 _rects.Add(new(Arena.Center, 20f, 5f));
-                Arena.Bounds = new ArenaBoundsCustom([.. _rects]);
+                Arena.Bounds = new ArenaBoundsCustom([.. _rects], Offset: 0.5f);
             }
             // arena reset
             else if (state is 0x10000004u or 0x00080004u or 0x00800004u)
--- BossMod/Modules/Dawntrail/Savage/M09SVampFatale/M09SVampFatale.cs
+++ BossMod/Modules/Dawntrail/Savage/M09SVampFatale/M09SVampFatale.cs
@@ -1,8 +1,6 @@
 ﻿namespace BossMod.Dawntrail.Savage.M09SVampFatale;
 
 sealed class KillerVoice(BossModule module) : Components.RaidwideCast(module, (uint)AID.KillerVoice);
-//sealed class Hardcore(BossModule module) : Components.BaitAwayCast(module, (uint)AID.Hardcore, 6f);
-//sealed class HardcoreBig(BossModule module) : Components.BaitAwayCast(module, (uint)AID.HardcoreBig, 15f);
 sealed class Hardcore(BossModule module) : Components.GenericBaitAway(module, centerAtTarget: true, tankbuster: true)
 {
     public override void OnCastStarted(Actor caster, ActorCastInfo spell)
@@ -34,8 +32,6 @@ sealed class Hardcore(BossModule module) : Components.GenericBaitAway(module, ce
     }
 }
 sealed class VampStomp(BossModule module) : Components.SimpleAOEs(module, (uint)AID.VampStomp, 10f);
-// 3 if regular but not always, sometimes 4; 5 hits when 8 stacks? 1 more cast each time it's used?; stack based, 5 hits on 2nd cast with 8 satisfied stacks
-//sealed class BrutalRain(BossModule module) : Components.StackWithIcon(module, (uint)IconID.ShareMulti, (uint)AID.BrutalRain, 6f, 5.1d, 6, 8, 3);
 
 sealed class BrutalRain(BossModule module) : Components.StackWithIcon(module, (uint)IconID.ShareMulti, (uint)AID.BrutalRain, 6f, 5.1d, 6, 8, 5)
 {
@@ -66,10 +62,42 @@ sealed class BrutalRain(BossModule module) : Components.StackWithIcon(module, (u
 
 sealed class SadisticScreech(BossModule module) : Components.RaidwideCast(module, (uint)AID.SadisticScreech);
 sealed class Coffinmaker(BossModule module) : Components.Adds(module, (uint)OID.Coffinmaker);
-sealed class Coffinfiller(BossModule module) : Components.SimpleAOEGroups(module, [(uint)AID.CoffinfillerLong, (uint)AID.CoffinfillerMed, (uint)AID.CoffinfillerShort], new AOEShapeRect(32f, 2.5f), 2, riskyWithSecondsLeft: 2d);
-sealed class DeadWake(BossModule module) : Components.SimpleAOEs(module, (uint)AID.DeadWake, new AOEShapeRect(10f, 10f), riskyWithSecondsLeft: 2d);
-// change risky time only during Arena1 for AI melee uptime
-sealed class HalfMoon(BossModule module) : Components.SimpleAOEGroups(module, [(uint)AID.HalfMoonLL, (uint)AID.HalfMoonRR, (uint)AID.HalfMoonBigLL, (uint)AID.HalfMoonBigRR, (uint)AID.HalfMoonLR, (uint)AID.HalfMoonRL, (uint)AID.HalfMoonBigLR, (uint)AID.HalfMoonBigRL], new AOEShapeCone(64f, 90.Degrees()), 1, 1);
+sealed class Coffinfiller(BossModule module) : Components.SimpleAOEGroups(module, [(uint)AID.CoffinfillerLong, (uint)AID.CoffinfillerMed, (uint)AID.CoffinfillerShort], new AOEShapeRect(32f, 2.5f), 2, riskyWithSecondsLeft: 2.5d);
+sealed class DeadWake(BossModule module) : Components.SimpleAOEs(module, (uint)AID.DeadWake, new AOEShapeRect(10f, 10f), riskyWithSecondsLeft: 2.5d);
+//sealed class HalfMoon(BossModule module) : Components.SimpleAOEGroups(module, [(uint)AID.HalfMoonLL, (uint)AID.HalfMoonRR, (uint)AID.HalfMoonBigLL, (uint)AID.HalfMoonBigRR, (uint)AID.HalfMoonLR, (uint)AID.HalfMoonRL, (uint)AID.HalfMoonBigLR, (uint)AID.HalfMoonBigRL], new AOEShapeCone(64f, 90.Degrees()), 1, 1);
+// change risky time only during Arena1 and only for melee classes for AI melee uptime; if ranges are too far out and risky is short they can get hit
+sealed class HalfMoon(BossModule module) : Components.SimpleAOEGroups(module, [(uint)AID.HalfMoonLL, (uint)AID.HalfMoonRR, (uint)AID.HalfMoonBigLL, (uint)AID.HalfMoonBigRR, (uint)AID.HalfMoonLR, (uint)AID.HalfMoonRL, (uint)AID.HalfMoonBigLR, (uint)AID.HalfMoonBigRL], new AOEShapeCone(64f, 90.Degrees()), 1, 1)
+{
+    private readonly ArenaChanges _arena = module.FindComponent<ArenaChanges>()!;
+    public override ReadOnlySpan<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        var count = Casters.Count;
+        if (count == 0)
+        {
+            return [];
+        }
+
+        var aoes = CollectionsMarshal.AsSpan(Casters);
+        var max = count > MaxCasts ? MaxCasts : count;
+
+        if (_arena.Active)
+        {
+            var isMelee = actor.Role is Role.Tank or Role.Melee;
+
+            if (isMelee)
+            {
+                var time = WorldState.CurrentTime;
+
+                for (var i = 0; i < max; ++i)
+                {
+                    ref var aoe = ref aoes[i];
+                    aoe.Risky = aoe.Activation.AddSeconds(-2.5d) <= time;
+                }
+            }
+        }
+        return aoes[..max];
+    }
+}
 sealed class CrowdKill(BossModule module) : Components.RaidwideCastDelay(module, (uint)AID.CrowdKillCast, (uint)AID.CrowdKill, 5.9f);
 sealed class FinaleFatale(BossModule module) : Components.RaidwideCastsDelay(module, [(uint)AID.FinaleFataleCast, (uint)AID.FinaleFataleBigCast], [(uint)AID.FinaleFatale, (uint)AID.FinaleFataleBig], 6.2f);
 sealed class PulpingPulse(BossModule module) : Components.SimpleAOEs(module, (uint)AID.PulpingPulse, 5f);
@@ -92,7 +120,7 @@ GroupType = BossModuleInfo.GroupType.CFC,
 GroupID = 1069u,
 NameID = 14300u,
 SortOrder = 1,
-PlanLevel = 0)]
+PlanLevel = 90)]
 public sealed class M09SVampFatale(WorldState ws, Actor primary) : BossModule(ws, primary, ArenaCenter, ArenaBounds)
 {
     public static readonly WPos ArenaCenter = new(100f, 100f);
