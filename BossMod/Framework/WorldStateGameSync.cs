03bda800392974df87c0391c80dd548d80e3f5c0
CarnifexOptimus <156172553+CarnifexOptimus@users.noreply.github.com>
2026-01-27 16:54:02 +0100

    fix merge error

--- BossMod/Framework/WorldStateGameSync.cs
+++ BossMod/Framework/WorldStateGameSync.cs
@@ -171,18 +171,7 @@ sealed class WorldStateGameSync : IDisposable
         _processLegacyMapEffectHook.Enable();
         Service.Log($"[WSG] LegacyMapEffect address = {_processLegacyMapEffectHook.Address:X}");
 
-<<<<<<< HEAD
         _inventoryAckHook = Service.Hook.HookFromSignature<InventoryAckDelegate>("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 48 8D 57 10 8B CE E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 48 8B D7", InventoryAckDetour);
-=======
-        _applyKnockbackHook = Service.Hook.HookFromSignature<ApplyKnockbackDelegate>("E8 ?? ?? ?? ?? 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? FF C6", ApplyKnockbackDetour);
-        //if (Service.IsDev)
-        //{
-        //    _applyKnockbackHook.Enable();
-        //    Service.Log($"[WSG] ApplyKnockback address = {_applyKnockbackHook.Address:X}");
-        //}
-
-        _inventoryAckHook = Service.Hook.HookFromSignature<InventoryAckDelegate>("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 48 8D 57 10 41 8B CE E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 48 8B D7", InventoryAckDetour);
->>>>>>> merge
         _inventoryAckHook.Enable();
         Service.Log($"[WSG] InventoryAck address = {_inventoryAckHook.Address:X}");
 
