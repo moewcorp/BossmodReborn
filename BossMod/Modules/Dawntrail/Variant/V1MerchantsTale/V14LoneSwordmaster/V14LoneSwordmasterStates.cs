cc718904ed0d584d219939b67a8c5e1161bfd08a
gynorhino <typemoondb@gmail.com>
2026-03-07 12:50:31 -0500

    Merchants Tale WIP

---
+++ BossMod/Modules/Dawntrail/Variant/V1MerchantsTale/V14LoneSwordmaster/V14LoneSwordmasterStates.cs
@@ -0,0 +1,32 @@
+﻿namespace BossMod.Dawntrail.VariantCriterion.V1MerchantsTale.V14LoneSwordmaster;
+
+class V14LoneSwordmasterStates : StateMachineBuilder
+{
+    public V14LoneSwordmasterStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<DebuffTracker>()
+            .ActivateOnEnter<LashOfLight>()
+            .ActivateOnEnter<WillOfTheUnderworld>()
+            .ActivateOnEnter<EarthRendingEight>()
+            .ActivateOnEnter<EarthRendingEightCross>()
+            .ActivateOnEnter<WaitingWounds>()
+            .ActivateOnEnter<HeavensConfluence>()
+            .ActivateOnEnter<HeavensConfluenceIcon>()
+            .ActivateOnEnter<CrusherOfLions>()
+            .ActivateOnEnter<UnyieldingWill>()
+            .ActivateOnEnter<SteelsbreathRelease>()
+            .ActivateOnEnter<SteelsbreathRelease1>()
+            .ActivateOnEnter<Concentrativity>()
+            .ActivateOnEnter<ConcentrativityKnockback>()
+            .ActivateOnEnter<ConcentravityMagnetFloor>()
+            .ActivateOnEnter<ConcentrativityRocks>()
+            .ActivateOnEnter<MawOfTheWolf>()
+            .ActivateOnEnter<StingOfTheScorpion>()
+            .ActivateOnEnter<PlummetSmall>()
+            .ActivateOnEnter<Plummet>()
+            .ActivateOnEnter<PlummetBig>()
+            .ActivateOnEnter<PlummetProximity>()
+            .ActivateOnEnter<WillOfTheUnderworldRocks>();
+    }
+}
