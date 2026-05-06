f63188da8ac70378f54e7d8fe3f5197f1bece622
NiGuangOwO <m2589895500@gmail.com>
2026-05-06 20:22:07 +0800

    1

--- BossMod/Framework/Service.cs
+++ BossMod/Framework/Service.cs
@@ -38,7 +38,7 @@ public sealed class Service
     public static void LogVerbose(string msg) => LogHandlerVerbose?.Invoke(msg);
 
     public static Lumina.GameData? LuminaGameData;
-    public static Lumina.Excel.ExcelSheet<T>? LuminaSheet<T>() where T : struct, Lumina.Excel.IExcelRow<T> => LuminaGameData?.GetExcelSheet<T>(Lumina.Data.Language.English);
+    public static Lumina.Excel.ExcelSheet<T>? LuminaSheet<T>() where T : struct, Lumina.Excel.IExcelRow<T> => LuminaGameData?.GetExcelSheet<T>(Lumina.Data.Language.ChineseSimplified);
     public static T? LuminaRow<T>(uint row) where T : struct, Lumina.Excel.IExcelRow<T> => LuminaSheet<T>()?.GetRowOrDefault(row);
     public static ConcurrentDictionary<Lumina.Text.ReadOnly.ReadOnlySeString, Lumina.Text.ReadOnly.ReadOnlySeString> LuminaRSV = []; // TODO: reconsider
 
