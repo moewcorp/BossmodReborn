2f4c7006aadf10354aa2cf38f988fb83062c3138
gynorhino <typemoondb@gmail.com>
2026-08-25 01:51:13 -0400

    FTMEx boss 1 WIP, boss 2 3 4 stubs

--- BossMod/Modules/Dawntrail/Foray/ForkedTowerMagic/Extreme/FTME3Necrophobia/FTME3Necrophobia.cs
+++ BossMod/Modules/Dawntrail/Foray/ForkedTowerMagic/Extreme/FTME3Necrophobia/FTME3Necrophobia.cs
@@ -1,9 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace BossMod.Dawntrail.Foray.ForkedTowerMagic.Extreme.FTME3Necrophobia;
 
-namespace BossModReborn.Modules.Dawntrail.Foray.ForkedTowerMagic.Extreme.FTME3Necrophobia;
-
-internal class FTME3Necrophobia
+[ModuleInfo(BossModuleInfo.Maturity.Dummy,
+StatesType = typeof(FTME3NecrophobiaStates),
+ConfigType = null, // replace null with typeof(NecrophobiaConfig) if applicable
+ObjectIDType = typeof(OID),
+ActionIDType = typeof(AID), // replace null with typeof(AID) if applicable
+StatusIDType = typeof(SID), // replace null with typeof(SID) if applicable
+TetherIDType = typeof(TetherID), // replace null with typeof(TetherID) if applicable
+IconIDType = typeof(IconID), // replace null with typeof(IconID) if applicable
+PrimaryActorOID = (uint)OID.Necrophobia,
+Contributors = "",
+Expansion = BossModuleInfo.Expansion.Dawntrail,
+Category = BossModuleInfo.Category.Foray,
+GroupType = BossModuleInfo.GroupType.TheForkedTowerMagic,
+GroupID = 1114u,
+NameID = 14503u,
+SortOrder = 3,
+PlanLevel = 100)]
+[SkipLocalsInit]
+public sealed class FTME3Necrophobia(WorldState ws, Actor primary) : BossModule(ws, primary, new(100f, 800f), new ArenaBoundsCircle(24f))
 {
+    protected override bool CheckPull() => base.CheckPull() && Raid.Player()!.Position.InCircle(Arena.Center, 24f);
 }
