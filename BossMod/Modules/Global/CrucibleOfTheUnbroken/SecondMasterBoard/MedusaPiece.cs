23205fc6fd240fe47565523324426c8e5d46ef51
Equilius <equiliuschamberlain@gmail.com>
2026-09-21 12:57:05 +0100

    Fix: priority for add

--- BossMod/Modules/Global/CrucibleOfTheUnbroken/SecondMasterBoard/MedusaPiece.cs
+++ BossMod/Modules/Global/CrucibleOfTheUnbroken/SecondMasterBoard/MedusaPiece.cs
@@ -418,7 +418,7 @@ public sealed class MedusaPiece(WorldState ws, Actor primary) : BossModule(ws, p
             var e = hints.PotentialTargets[i];
             e.Priority = e.Actor.OID switch {
                 (uint)OID.LamiaPiece => 3,
-                (uint)OID.CyclopsPiece => e.Actor.FindStatus((uint)SID.StoneCurse) != null ? 6 : 1,
+                (uint)OID.CyclopsPiece => e.Actor.FindStatus((uint)SID.StoneCurse) != null ? 6 : 2,
                 (uint)OID.MedusaPiece => 1,
                 _ => 0
             };
